6cadbb826e6bdeff6ac6e76889c9ee99c128b9d9
d.granica <dimagranitsa@mail.ru>
2023-01-11 14:33:03 +0300

    add module with behavioral pattern memento

--- momento/src/main/java/org/example/Main.java
+++ momento/src/main/java/org/example/Main.java
@@ -1,7 +1,32 @@
 package org.example;
 
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws InterruptedException {
+        Project project = new Project();
+        GithabRepo github = new GithabRepo();
+
+        System.out.println("Creating new project. Version 1.0");
+        project.setVersionAndDate("Version 1/0");
+
+        System.out.println(project);
+
+        System.out.println("Saving current version to github...");
+        github.setSave(project.save());
+
+        System.out.println("Writing poor code...");
+
+        Thread.sleep(5000);
+        project.setVersionAndDate("Version 1.1");
+
+        System.out.println(project);
+
+        System.out.println("Something went wrong");
+
+        System.out.println("Rolling back to Version 1.0");
+        project.load(github.getSave());
+
+        System.out.println("Project after rollback");
+        System.out.println(project);
+
     }
 }
